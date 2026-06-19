21363ac0b1e3caa9c12928d4fcc8df21bb9670a4
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-06-19 15:11:30 +0530

    Update create_tables.py

--- database/create_tables.py
+++ database/create_tables.py
@@ -1,13 +1,39 @@
 import asyncio
+import logging
 
 from database.db import engine
 from database.models import Base
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
+
+
 async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(
-            Base.metadata.create_all
+
+    try:
+
+        async with engine.begin() as conn:
+
+            await conn.run_sync(
+                Base.metadata.create_all
+            )
+
+        logging.info(
+            "✅ Database Tables Created Successfully"
         )
 
-asyncio.run(create_tables())
+    except Exception as e:
+
+        logging.error(
+            f"❌ Database Error: {e}"
+        )
+
+
+if __name__ == "__main__":
+
+    asyncio.run(
+        create_tables()
+    )
