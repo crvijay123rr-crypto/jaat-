a1096c2ee3ad4751448bdeeb943a607463976126
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-06-19 15:35:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,24 @@ ADMINS = [
     if x.strip()
 ]
 
-BACKUP_CHAT_ID = int(os.getenv("BACKUP_CHAT_ID"))
+BACKUP_CHAT_ID = int(
+    os.getenv("BACKUP_CHAT_ID", "0")
+)
 
-TRIAL_DAYS = int(os.getenv("TRIAL_DAYS", 1))
-TRIAL_FILES = int(os.getenv("TRIAL_FILES", 10))
+BOT_USERNAME = os.getenv(
+    "BOT_USERNAME",
+    ""
+)
+
+SUPPORT_USERNAME = os.getenv(
+    "SUPPORT_USERNAME",
+    ""
+)
+
+TRIAL_DAYS = int(
+    os.getenv("TRIAL_DAYS", "1")
+)
+
+TRIAL_FILES = int(
+    os.getenv("TRIAL_FILES", "10")
+)
