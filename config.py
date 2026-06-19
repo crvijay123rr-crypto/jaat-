1d0c0713e8bb1a1148e8ec22a9afa6d60a5392f7
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-06-19 14:56:08 +0530

    Add configuration for environment variables

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+ADMINS = [
+    int(x)
+    for x in os.getenv("ADMINS", "").split(",")
+    if x.strip()
+]
+
+BACKUP_CHAT_ID = int(os.getenv("BACKUP_CHAT_ID"))
+
+TRIAL_DAYS = int(os.getenv("TRIAL_DAYS", 1))
+TRIAL_FILES = int(os.getenv("TRIAL_FILES", 10))
